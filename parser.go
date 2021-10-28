1bb2280fb6d6bedfe591c0e089d9e1de8824c11d
David Jackson <dwjackson@users.noreply.github.com>
2021-10-28 09:08:18 -0400

    Parse quantity-less ingredients

--- parser.go
+++ parser.go
@@ -1,22 +1,43 @@
 package cuisiner
 
-import "strings"
+import (
+	"strings"
+	"regexp"
+)
 
 type Recipe struct {
+	Ingredients []string
 	Directions []string
 }
 
 func Parse(input string) (*Recipe, error) {
+	var ingredients []string
 	var directions []string
 	lines := strings.Split(input, "\n")
 	for _, line := range lines {
 		trimmedLine := strings.TrimSpace(line)
 		if len(trimmedLine) > 0 {
+			lineIngredients := discoverIngredients(line)
+			for _, ingredient := range lineIngredients {
+				ingredients = append(ingredients, ingredient)
+			}
 			directions = append(directions, line)
 		}
 	}
 	recipe := &Recipe{
+		Ingredients: ingredients,
 		Directions: directions,
 	}
 	return recipe, nil
+}
+
+func discoverIngredients(line string) []string {
+	var ingredients []string
+	re := regexp.MustCompile(`\@\w+`)
+	matches := re.FindAllString(line, -1)
+	for _, m := range matches {
+		name := m[1:]
+		ingredients = append(ingredients, name)
+	}
+	return ingredients
 }
--- parser_test.go
+++ parser_test.go
@@ -31,6 +31,37 @@ func TestSkipBlankLines(t *testing.T) {
 	}
 }
 
+func TestIngredientWithoutQuantity(t *testing.T) {
+	input := "Chop up a @potato and set aside"
+	recipe, err := Parse(input)
+	if err != nil || len(recipe.Directions) != 1 {
+		t.Fatalf("Expected 1 direction")
+	}
+	if len(recipe.Ingredients) != 1 {
+		t.Fatalf("Expected 1 ingredient")
+	}
+	if recipe.Ingredients[0] != "potato" {
+		t.Fatalf("Expected ingredient potato, got %q", recipe.Ingredients[0])
+	}
+}
+
+func TestTwoIngredientsWithoutQuantity(t *testing.T) {
+	input := "Chop up a @potato and a @leek and set aside"
+	recipe, err := Parse(input)
+	if err != nil {
+		t.Fatalf("Parse failed")
+	}
+	if len(recipe.Ingredients) != 2 {
+		t.Fatalf("Expected 2 ingredients")
+	}
+	if recipe.Ingredients[0] != "potato" {
+		t.Fatalf("Expected ingredient potato, got %q", recipe.Ingredients[0])
+	}
+	if recipe.Ingredients[1] != "leek" {
+		t.Fatalf("Expected ingredient leek, got %q", recipe.Ingredients[1])
+	}
+}
+
 func testDirection(t *testing.T, expected string, actual string) {
 	if actual != expected {
 		t.Fatalf("Expected %q, got %q", expected, actual)
