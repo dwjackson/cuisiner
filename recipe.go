6bf3e575d08d96b9be2544febe226fbcf39cecd8
David Jackson <dwjackson@users.noreply.github.com>
2021-10-29 11:04:51 -0400

    Move Timer struct to its own file

--- recipe.go
+++ recipe.go
@@ -1,7 +1,5 @@
 package main
 
-import "strings"
-
 type Recipe struct {
 	Ingredients []Ingredient
 	Directions  []string
@@ -11,40 +9,4 @@ type Recipe struct {
 
 func (r *Recipe) IngredientsList() []Ingredient {
 	return r.Ingredients
-}
-
-type Timer struct {
-	Duration float64
-	Unit     string
-}
-
-func (t1 *Timer) Add(t2 *Timer) Timer {
-	var duration float64
-	var unit string
-	if t1.Unit == t2.Unit {
-		unit = t1.Unit
-		duration = t1.Duration + t2.Duration
-	} else {
-		d1 := durationToMinutes(t1)
-		d2 := durationToMinutes(t2)
-		duration = d1 + d2
-		unit = "minutes"
-	}
-	return Timer{
-		Duration: duration,
-		Unit:     unit,
-	}
-}
-
-func durationToMinutes(t *Timer) float64 {
-	if strings.HasPrefix(t.Unit, "min") {
-		return t.Duration
-	}
-	if strings.HasPrefix(t.Unit, "sec") {
-		return t.Duration / 60
-	}
-	if strings.HasPrefix(t.Unit, "hour") {
-		return t.Duration * 60
-	}
-	return 0
 }
